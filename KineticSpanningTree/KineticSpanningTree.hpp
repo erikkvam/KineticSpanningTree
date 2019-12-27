dde312a8a029d1ee3702adeeece8586092455bfe
erikkvam <erik.kvam@me.com>
2019-12-27 12:30:57 +0100

    Basic data structure rehaul

--- KineticSpanningTree/KineticSpanningTree.hpp
+++ KineticSpanningTree/KineticSpanningTree.hpp
@@ -14,8 +14,46 @@
 
 class KineticSpanningTree {
 private:
-    Graph MST;
-    KineticGraph kineticGraph;
+    struct vertex {
+        string label = "";
+        int id;
+    };
+    struct edge {
+        int id;
+        int v, w; //vertex IDs
+        double a, b; //cost = a+bt
+    };
+    
+    enum EventType {VertexDeletion, EdgeDeletion, EdgeWeightUpdate, VertexAddition, EdgeAddition, MSTSwap};
+    struct Event {
+        /*
+         VertexDeletion  : delete vertex v
+         EdgeDeletion    : delete edge v-w
+         EdgeWeightUpdate: set vertex v-w to weight a+bt
+         VertexAddition  : add vertex v
+         EdgeAddition    : add edge v-w
+         MSTSwap         : swap edges v-w and a-b in the MST
+         */
+        EventType type;
+        int time;
+        int v, w, a, b;
+    }
+    
+    vector<vertex> vertices;
+    vector<edge> edges;
+    vector<int> MSTedges;
+    
+    vector<Event>; //should act as a priority queue
+    
+    typedef vector<vertex> cluster;
+    vector<cluster> clusters;
+    
+    struct swap {
+        int v, w, x, y; //delete v-w and add x-y
+    }
+    swap nextIntra, nextInter, nextDual;
+
+    
 
     KineticSpanningTree();
     
@@ -25,5 +63,6 @@ protected:
 public:
     KineticSpanningTree(string name);
     void print();
+    void advanceTimeUntil(float t);
 };
 #endif /* KineticSpanningTree_hpp */
