9aaf8e769ad6f83878431b49a59b78f48bc0d3dc
erikkvam <erik.kvam@me.com>
2020-01-29 21:00:08 +0100

    Implement add

--- KineticSpanningTree/Graph.cpp
+++ KineticSpanningTree/Graph.cpp
@@ -7,3 +7,18 @@
 //
 
 #include "Graph.hpp"
+bool Graph::add(const int& u, const int& v, const int& a, const int& b){
+    if (inV(u) and inV(v) and not inE(u,v)) {
+        pair<int,int> ab = pair<int,int>(a,b);
+        
+        iterator uPos = Ew.begin() + u;
+        *uPos.insert(pair<int, pair<int,int>>(v,ab));
+        
+        iterator vPos = Ew.begin() + v;
+        *vPos.insert(pair<int, pair<int,int>>(u,ab));
+        
+        return true;
+    } else {
+        return false;
+    }
+}
