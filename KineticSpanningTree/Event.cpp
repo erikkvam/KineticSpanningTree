6ebc828d42eca8f95a6b131b57fdc492495b9249
erikkvam <erik.kvam@me.com>
2020-02-07 18:23:52 +0100

    Implement create[EventType]()

--- KineticSpanningTree/Event.cpp
+++ KineticSpanningTree/Event.cpp
@@ -7,3 +7,31 @@
 //
 
 #include "Event.hpp"
+
+Event Event::createAddition(float t, int u, int v, int a, int b){
+    Event returnable;
+    returnable.time = t;
+    returnable.type = Addition;
+    returnable.parameters[0] = u;
+    returnable.parameters[1] = v;
+    returnable.parameters[2] = a;
+    returnable.parameters[3] = b;
+    return returnable;
+}
+
+Event Event::createDeletion(const float t, const int& u, const int& v){
+    Event returnable = Event();
+    returnable.time = t;
+    returnable.type = Addition;
+    returnable.parameters[0] = u;
+    returnable.parameters[1] = v;
+    return returnable;
+}
+
+Event Event::createCertificateFailure(const float t, const Certificate cert){
+    Event returnable;
+    returnable.time = t;
+    returnable.type = Addition;
+    returnable.certificate = cert;
+    return returnable;
+}
